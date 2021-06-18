c9a5b8421a91a00444039b3e442fa2f581a8d036
Nikolay Galkin <mr.galkin@gmail.com>
2021-06-18 10:53:03 +0300

    v0.3.1
    * Tailwind 2.2
    * feat: ncu
    * refactor: remove pseudo elements package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const { coolGray } = require('tailwindcss/colors')
 
 module.exports = { /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
-
   mode: 'jit',
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
@@ -23,7 +22,6 @@ module.exports = { /** @type {import("@types/tailwindcss/tailwind-config").Tailw
     extend: {},
   },
   plugins: [
-    require('tailwindcss-pseudo-elements'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
